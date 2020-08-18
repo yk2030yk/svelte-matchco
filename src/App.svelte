bd35f12649fd12c8ed3176b0fcbbfaa9c3004078
yk2030yk <yk.ngsyk@gmail.com>
2020-08-19 08:50:30 +0900

    init

---
+++ src/App.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	const name = "world"
+</script>
+
+<main>
+	<h1>Hello {name}!</h1>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+</style>
